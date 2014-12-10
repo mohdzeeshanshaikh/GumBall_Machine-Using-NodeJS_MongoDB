79a26d4d17e8ea7de50be508079e5a13404b070c
Mohd Zeeshan Shaikh <zeemailed@gmail.com>
2014-12-10 00:25:37 +0000

    final changes

--- routes/gumMachine.js
+++ routes/gumMachine.js
@@ -1,17 +1,14 @@
-/**
- * New node file
- */
-
 var mongo = require('mongodb');
 var crypto = require('crypto');
-var key = 'V77q995EZX36DDYw0OTjBpW0587i7DM4';
+var key = 'B3734jjH6F36DDYw0OTjBpJKy65asth4';
+
 var create_hash = function(state,ts){
 	var text = state + ts + key;
-	hmac = crypto.createHmac("sha256", key);
-	hmac.setEncoding('base64');
-	hmac.write(text);
-	hmac.end();
-	hash = hmac.read();
+	var	hmac = crypto.createHmac("sha256", key);
+		hmac.setEncoding('base64');
+		hmac.write(text);
+		hmac.end();
+	var	hash = hmac.read();
 	return hash;
 }
 
@@ -20,7 +17,7 @@ Db = mongo.Db,
 BSON = mongo.BSONPure;
 
 var server = new Server('ds039950.mongolab.com',39950,{auto_reconnect: true});
-db = new Db('zee_cmpe281', server);
+var db = new Db('zee_cmpe281', server);
 
 db.open(function(err, db) {
   db.authenticate("zee","zee123",function(err,success){
@@ -53,7 +50,7 @@ exports.addMachine = function(req, res){
 exports.listMachines = function(req, res){
 	db.collection('NodeGumballMachine', function(err, collection) {
         collection.find().toArray(function(err, list) {
-        	 res.render('listMachines', {result:list});// res.send(items);
+        	 res.render('listMachines', {result:list});
         });
     });
 }
@@ -61,7 +58,6 @@ exports.listMachines = function(req, res){
 
 exports.getDetails = function(req, res){
 	var id = req.params.id;
-	console.log("id: " + id);
 	db.collection('NodeGumballMachine', function(err, collection){
 		collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, record){
 			var state = 'No Coin';
@@ -82,6 +78,7 @@ exports.updateMachine = function(req, res){
 	var now = new Date().getTime();
 	var diff = ((now-ts)/1000);
 	var hash_second = create_hash(state,ts);
+	var count=input.count;
 	
 	var data= {
 			id: id,
@@ -92,7 +89,7 @@ exports.updateMachine = function(req, res){
 	console.log(data);
 	
 	if(diff>120){
-		res.render('getDetails', {result: data, _id:id, state:state, ts:now,hash: hash, notify: 'Invalid Session'});
+		res.render('getDetails', {result: data, _id:id, state:state, ts:now,hash: hash_second, notify: 'Invalid Session'});
 	}
 	if(activity == "Insert Quarter"){
 		if(state == "No Coin"){
@@ -109,11 +106,16 @@ exports.updateMachine = function(req, res){
 			res.render('getDetails', {result: data, _id:id, state:state,ts:now, hash: create_hash(state, now), notify:'Please Insert Coin'});
 		}
 		else if(state == "Has Coin"){
-				if(input.count>0){
+				db.collection('NodeGumballMachine', function(err, collection){
+					collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, record){
+						count= record.count
+					});
+				});
+				if(count>0){
 					var data1 = {
 							serialNo: input.serialNo,
 							modelNo: input.modelNo,
-							count: input.count - 1
+							count: count - 1
 					}
 				db.collection('NodeGumballMachine', function(err, collection){
 					collection.update({'_id':new BSON.ObjectID(id)}, data1, {safe:true}, function(err, result) {
@@ -127,11 +129,10 @@ exports.updateMachine = function(req, res){
 			            }
 					});
 				});
-				}
-				else{
-					res.render('getDetails', {result: data, ts: now, hash: create_hash(state, now), state: state, notify:'No Inventory'});
-				}
+			}
+			else{
+				res.render('getDetails', {result: data, ts: now, hash: create_hash(state, now), state: state, notify:'No Inventory'});
+			}
 		}
 	}
-	
 }
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , gumMachine = require('./routes/gumMachine');
+var http = require('http')
+var path = require('path')
+var gumMachine = require('./routes/gumMachine');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port',process.env.OPENSHIFT_NODEJS_PORT || 3000)
+app.set('ip_address',process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1')
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -23,18 +17,15 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-//app.get('/',gumMachine.addMachine);
+app.get('/', gumMachine.listMachines);
 app.get('/getDetails/:id', gumMachine.getDetails);
 app.post('/getDetails/:id', gumMachine.updateMachine);
 app.get('/listMachines', gumMachine.listMachines);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), app.get('ip_address'),function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
